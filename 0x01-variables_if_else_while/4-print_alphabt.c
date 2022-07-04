8189adbd4eb556bd00b5b3e31cfc520a387b5941
Mapayi femi <olorunfemimapayi@gmail.com>
2022-07-04 04:10:40 -0700

    second commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= '2'; ch==)
+	{
+		if(ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
